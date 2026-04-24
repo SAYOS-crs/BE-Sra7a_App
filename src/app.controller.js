e9f45224402f902beab15c7b03ddd60bbc810fb2
Eslam Elawdy <eslam.mohamed.ki123@gmail.com>
2026-04-24 17:32:26 +0300

    refactor: revirced proxy X-Forwarded-For

--- src/app.controller.js
+++ src/app.controller.js
@@ -3,17 +3,53 @@ import { AuthRouter, MessageRouter, UserRouter } from "./Modules/index.js";
 import { GlobalError } from "./Utils/responses/error.respons.js";
 import path from "node:path";
 import { LogRecoreder } from "./Utils/logs/logs.js";
-import { rateLimit } from "express-rate-limit";
+import { ipKeyGenerator, rateLimit } from "express-rate-limit";
+import geoip from "geoip-lite";
+import { Radis } from "./DB/radis.connection.js";
 
 export const limiter = rateLimit({
-  windowMs: 3 * 60 * 1000, // 15 minutes
-  limit: 10, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-  standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
-  // store: ... , // Redis, Memcached, etc. See below.
+  windowMs: 3 * 60 * 1000, // 3 minutes
+  limit: async function (req) {
+    const { country } = geoip.lookup(req.ip) || {};
+    console.log(country);
+    return country == "EG" ? 10 : 1;
+  },
+
+  legacyHeaders: false,
+  handler: (req, res, next) => {
+    res.status(429).json({ message: "to many requsts , try again later" });
+  },
+
+  keyGenerator: (req, res, next) => {
+    const ip = ipKeyGenerator(req.ip, 56);
+
+    return `${ip}-${req.path}`;
+  },
+  store: {
+    incr: async (key, cb) => {
+      try {
+        const counter = await Radis.incr(key);
+
+        if (counter === 1) {
+          await Radis.expire(key, 60 * 3);
+        }
+
+        cb(null, counter);
+      } catch (error) {
+        cb(error, null);
+      }
+    },
+    decrement: async (key) => {
+      if (await Radis.exists(key)) {
+        await Radis.decr(key);
+      }
+    },
+  },
 });
 
 export default function Bootstrap(app, express) {
   // loger(app, "/Auth", AuthRouter, "auth.log");
+  app.set("trust proxy", true);
   app.use(limiter);
   app.use("/a", express.static(path.resolve("./src/Uploudes")));
   app.use("/Auth", LogRecoreder({ fileName: "Auth" }), AuthRouter);
